12826f10735bf73d1eb79fd4dc362c63c5318b80
John Dee <ekstrandcaleb@gmail.com>
2019-11-14 11:14:37 -0700

    saves

--- models/post.js
+++ models/post.js
@@ -1,20 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 const postSchema = new Schema({
   title: { type: String, required: true },
   body: { type: String, required: true },
   user: String,  
-<<<<<<< HEAD
- // comment: String,ORIGINAL
   comment: [{
     type: String
-  }] //TUTOR
-=======
-  comment: []
->>>>>>> 930aeb211e8f7af8b9ac84f23c451ebb65b67ccd
+  }]
 });
-
 const Post = mongoose.model("Post", postSchema);
-
 module.exports = Post;
